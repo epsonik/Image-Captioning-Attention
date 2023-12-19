375fc78e11f6cfda1465ee074d0ab11f3d618c41
mateuszb <mateusz.b@aibusters.com>
2023-12-20 00:24:14 +0100

    e

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from config import config
 
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
+
 def set_trainer():
     # data parameters
     data_folder = config.dataset_output_path
@@ -40,10 +41,10 @@ def set_trainer():
         if config.embed_pretrain == True:
             # load pre-trained word embeddings for words in the word map
             embeddings, embed_dim = load_embeddings(
-                emb_file = config.embed_path,
-                word_map = word_map,
-                output_folder = config.dataset_output_path,
-                output_basename = config.dataset_basename
+                emb_file=config.embed_path,
+                word_map=word_map,
+                output_folder=config.dataset_output_path,
+                output_basename=config.dataset_basename
             )
         else:
             # or initialize embedding weights randomly
@@ -56,17 +57,17 @@ def set_trainer():
         # optimizer for encoder's CNN (if fine-tune)
         if config.fine_tune_encoder:
             encoder_optimizer = optim.Adam(
-                params = filter(lambda p: p.requires_grad, encoder.CNN.parameters()),
-                lr = config.encoder_lr
+                params=filter(lambda p: p.requires_grad, encoder.CNN.parameters()),
+                lr=config.encoder_lr
             )
         else:
             encoder_optimizer = None
 
         # ----------------- decoder ------------------
         decoder = models.decoders.make(
-            vocab_size = len(word_map),
-            embed_dim = embed_dim,
-            embeddings = embeddings
+            vocab_size=len(word_map),
+            embed_dim=embed_dim,
+            embeddings=embeddings
         )
         # optimizer for decoder
 
@@ -75,26 +76,25 @@ def set_trainer():
         # print(len(decoder_params))
         if caption_model == 'adaptive_att' or caption_model == 'spatial_att':
             decoder_params = decoder_params + list(encoder.global_mapping.parameters()) \
-                                            + list(encoder.spatial_mapping.parameters())
+                             + list(encoder.spatial_mapping.parameters())
         elif caption_model == 'show_tell':
             decoder_params = decoder_params + list(encoder.output_layer.parameters())
 
         decoder_optimizer = optim.Adam(
-            params = decoder_params,
-            lr = config.decoder_lr
+            params=decoder_params,
+            lr=config.decoder_lr
         )
 
     # or load checkpoint
     else:
-        encoder,
-        encoder_optimizer,
-        decoder,
-        decoder_optimizer,
-        start_epoch,
-        epochs_since_improvement,
-        best_bleu4,
+        encoder, \
+        encoder_optimizer, \
+        decoder, \
+        decoder_optimizer, \
+        start_epoch, \
+        epochs_since_improvement, \
+        best_bleu4, \
         caption_model = load_checkpoint(config.checkpoint, config.fine_tune_encoder, config.encoder_lr)
-
     # move to GPU, if available
     decoder = decoder.to(device)
     encoder = encoder.to(device)
@@ -105,52 +105,52 @@ def set_trainer():
     # custom dataloaders
     # normalizacja za pomoca sredniej i odchylenia standardowego
     normalize = transforms.Normalize(
-        mean = [0.485, 0.456, 0.406],
-        std = [0.229, 0.224, 0.225]
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
     )
     train_loader = DataLoader(
         CaptionDataset(
             data_folder, data_name, 'train',
-            transform = transforms.Compose([normalize])
+            transform=transforms.Compose([normalize])
         ),
-        batch_size = config.batch_size,
-        shuffle = True,
-        num_workers = config.workers,
-        pin_memory = True
+        batch_size=config.batch_size,
+        shuffle=True,
+        num_workers=config.workers,
+        pin_memory=True
     )
     # transforms.Compose laczy kilka normalizacji ze soba
     val_loader = DataLoader(
         CaptionDataset(
             data_folder, data_name, 'val',
-            transform = transforms.Compose([normalize])
+            transform=transforms.Compose([normalize])
         ),
-        batch_size = config.batch_size,
-        shuffle = True,
-        num_workers = config.workers,
-        pin_memory = True
+        batch_size=config.batch_size,
+        shuffle=True,
+        num_workers=config.workers,
+        pin_memory=True
     )
 
     trainer = Trainer(
-        caption_model = caption_model,
-        epochs = config.epochs,
-        device = device,
-        word_map = word_map,
-        rev_word_map = rev_word_map,
-        start_epoch = start_epoch,
-        epochs_since_improvement = epochs_since_improvement,
-        best_bleu4 = best_bleu4,
-        train_loader = train_loader,
-        val_loader = val_loader,
-        encoder = encoder,
-        decoder = decoder,
-        encoder_optimizer = encoder_optimizer,
-        decoder_optimizer = decoder_optimizer,
-        loss_function = loss_function,
-        grad_clip = config.grad_clip,
-        tau = config.tau,
-        fine_tune_encoder = config.fine_tune_encoder,
-        tensorboard = config.tensorboard,
-        log_dir = config.log_dir
+        caption_model=caption_model,
+        epochs=config.epochs,
+        device=device,
+        word_map=word_map,
+        rev_word_map=rev_word_map,
+        start_epoch=start_epoch,
+        epochs_since_improvement=epochs_since_improvement,
+        best_bleu4=best_bleu4,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        encoder=encoder,
+        decoder=decoder,
+        encoder_optimizer=encoder_optimizer,
+        decoder_optimizer=decoder_optimizer,
+        loss_function=loss_function,
+        grad_clip=config.grad_clip,
+        tau=config.tau,
+        fine_tune_encoder=config.fine_tune_encoder,
+        tensorboard=config.tensorboard,
+        log_dir=config.log_dir
     )
 
     return trainer
