e71977ace62ff92197efa911a8d75ddaa9416fac
mateuszb <mateusz.b@aibusters.com>
2024-01-04 17:30:57 +0100

    inception

--- config.py
+++ config.py
@@ -10,13 +10,13 @@ class config:
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
     caption_model = 'att2all'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
                                     # refer to README.md for more info about each model
-    output_path = "data/output/Resnet101_glove300_fte_true/"
+    output_path = "data/output/DenseNet201_glove300_fte_false/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/home/data/Images/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/home/data/Images/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'Resnet101_glove300_fte_true'  # any name you want
+    dataset_basename = 'DenseNet201_glove300_fte_false'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -39,13 +39,13 @@ class config:
     decoder_dim = 128  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'Resnet101_glove300_fte_true'  # any name you want
+    model_basename = 'DenseNet201_glove300_fte_false'  # any name you want
 
     # training parameters
     epochs = 100
     batch_size = 50
-    pretrained_encoder = 'Resnet101' #DenseNet201 InceptionV3 Resnet101
-    fine_tune_encoder = True  # fine-tune encoder or not
+    pretrained_encoder = 'DenseNet201' #DenseNet201 InceptionV3 Resnet101
+    fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
