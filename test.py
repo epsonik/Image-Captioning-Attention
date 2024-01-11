28477504ab6527beb8b299fd8cb0e761fd456c70
mateuszb <mateusz.b@aibusters.com>
2024-01-11 02:07:36 +0100

    test change

--- test.py
+++ test.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     for data_name in output_path:
         # path to save checkpoints
         model_path = os.path.join(config.base_path, "output", data_name, "checkpoints")
-        checkpoint = os.path.join('best_checkpoint_' + data_name + '.pth.tar')  # model checkpoint
+        checkpoint = os.path.join(model_path, 'best_checkpoint_' + data_name + '.pth.tar')  # model checkpoint
         print(checkpoint)
         beam_size = 2
         # load model
