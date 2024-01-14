bb6451ca785899ac076ed2cc8fd545e43b72f214
mateuszb <mateusz.b@aibusters.com>
2024-01-14 01:22:52 +0100

    show_tell_DenseNet201_glove300_fte_true

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ class config:
 
     # training parameters
     epochs = 50
-    batch_size = 400
+    batch_size = 300
     pretrained_encoder = 'DenseNet201' #DenseNet201 Resnet101
     fine_tune_encoder = True  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
