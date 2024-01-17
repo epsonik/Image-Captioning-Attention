6d5e0ccb0266f51bc3a84c083f8ed86fdf2edf09
mateuszb <mateusz.b@aibusters.com>
2024-01-17 02:10:15 +0100

    print model structure

--- print_model.py
+++ print_model.py
@@ -2,3 +2,5 @@ import torchinfo
 import torchvision
 regnet = torchvision.models.regnet_y_32gf(weights="IMAGENET1K_SWAG_E2E_V1")
 print(torchinfo.summary(regnet, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+resnet = torchvision.models.resnet101(pretrained = True)
+print(torchinfo.summary(resnet, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
