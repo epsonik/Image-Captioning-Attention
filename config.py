002f4fcf0641986164fab2207001642864666e8a
mateuszb <mateusz.b@aibusters.com>
2024-01-19 17:53:02 +0100

    cider evaluation

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ class config:
     # training parameters
     validation_measure = 'cider'  # bleu_4 cider
     epochs = 2
-    batch_size = 500
+    batch_size = 400
     pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32
     fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
