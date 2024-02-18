0c201056bd0f10cdcb777f046a43cfbd616ad1a5
mateuszb <mateusz.b@aibusters.com>
2024-02-18 14:49:01 +0100

    print model

--- print_model.py
+++ print_model.py
@@ -1,10 +1,10 @@
 import torchinfo
 import torchvision
-regnet32 = torchvision.models.regnet_y_32gf(weights="IMAGENET1K_SWAG_E2E_V1")
-print(torchinfo.summary(regnet32, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
-resnet = torchvision.models.resnet101(pretrained = True)
-print(torchinfo.summary(resnet, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+resnet152= torchvision.models.resnet152(weights="IMAGENET1K_V2")
+print(torchinfo.summary(resnet152, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+densenet161 = torchvision.models.densenet161(pretrained=True)
+print(torchinfo.summary(densenet161, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 
 
-regnet16 = torchvision.models.regnet_y_16gf(weights="IMAGENET1K_SWAG_E2E_V1")
-print(torchinfo.summary(regnet16, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+densenet121 = torchvision.models.densenet121(pretrained=True)
+print(torchinfo.summary(densenet121, (3, 224, 224), batch_dim=0, col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
