399e749a5e335c6e9cbf39f332d2ab5b5cd8db2d
mateuszb <mateusz.b@aibusters.com>
2024-02-20 00:11:30 +0100

    config

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ class config:
     epochs = 5
     batch_size = 5
     pretrained_encoder = 'DenseNet121'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
-    fine_tune_encoder = True  # fine-tune encoder or not
+    fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
