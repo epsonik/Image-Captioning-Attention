8e55e148226b23e826aebe687e27b8576527e467
mateuszb <mateusz.b@aibusters.com>
2024-02-19 23:35:12 +0100

    print

--- print_model.py
+++ print_model.py
@@ -1,6 +1,6 @@
 import torchinfo
 import torchvision
-
+print("resnet")
 resnet152 = torchvision.models.resnet152(weights="IMAGENET1K_V2")
 print(torchinfo.summary(resnet152, (3, 224, 224), batch_dim=0,
                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
