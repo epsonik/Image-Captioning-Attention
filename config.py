0318bb0143d0c503599d7166dc72b6674828c715
mateuszb <mateusz.b@aibusters.com>
2024-03-07 16:54:42 +0100

    Resnet152_decoder_dim_512_fine_tune_embeddings_true

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class config:
     output_path = "data/output/Resnet152_decoder_dim_512_fine_tune_embeddings_true/"
     dataset_type = 'coco'
     # dataset parameters
-    dataset_image_path = os.path.join(base_path, '/home/dane/Images/coco2014/')
-    dataset_caption_path = os.path.join(base_path, '/home/dane/Images/coco2014/karpathy/dataset_coco.json')
+    dataset_image_path = os.path.join(base_path, '/home2/data/images/coco2014/')
+    dataset_caption_path = os.path.join(base_path, '/home2/data/images/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
     dataset_basename = 'Resnet152_decoder_dim_512_fine_tune_embeddings_true'  # any name you want
 
@@ -29,7 +29,7 @@ class config:
     embed_pretrain = True  # false: initialize embedding weights randomly
     # true: load pre-trained word embeddings
     embed_path = os.path.join(base_path,
-                              '/home/dane/Images/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
+                              '/home2/data/images/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
     embed_dim = 512  # dimension of word embeddings
     # only makes sense when `embed_pretrain = False`
     fine_tune_embeddings = True  # fine-tune word embeddings?
