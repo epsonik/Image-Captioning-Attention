33e29c44d060273665ec9c3fc8bba21eb45d0e6d
mateuszb <mateusz.b@aibusters.com>
2024-03-09 15:06:52 +0100

    Resnet152_decoder_dim_512_fine_tune_embeddings_true evaluation

--- test2.py
+++ test2.py
@@ -18,7 +18,7 @@ from config import config
 import pathlib
 
 device = torch.device(
-    "cuda:1" if torch.cuda.is_available() else "cpu")
+    "cuda:2" if torch.cuda.is_available() else "cpu")
 data_f = os.path.join(config.base_path, "data")
 # word map, ensure it's the same the data was encoded with and the model was trained with
 word_map_file = os.path.join(data_f, "evaluation", 'wordmap' + '.json')
