d33f8c7c9c3770b643fc58b041f6b795cde65aa2
mateuszb <mateusz.b@aibusters.com>
2024-04-17 21:55:31 +0200

    inceptionV3

--- models/encoders/inception.py
+++ models/encoders/inception.py
@@ -24,12 +24,13 @@ class Inceptionv3(nn.Module):
 
         # pretrained ResNet-101 model (on ImageNet)
         inception = torchvision.models.inception_v3(pretrained=True)
+        inception.aux_logits = False
+        inception.AuxLogits = None
         modules = list(inception.children())[:-3]
-        self.resnet = nn.Sequential(*modules)
+        self.inception = nn.Sequential(*modules)
 
         self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
-        self.inception.aux_logits = False
-        self.inception.AuxLogits = None
+
 
 
 
