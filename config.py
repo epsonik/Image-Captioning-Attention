38d6daff176a2b6141e9fdcf3401920970f7027b
mateuszb <mateusz.b@aibusters.com>
2024-04-21 18:31:57 +0200

    inceptionv3

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ class config:
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
     caption_model = 'show_tell'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/InceptionV3_decoder_dim_512_fine_tune_encoder_tru_fine_tune_embeddings_false/"
+    output_path = "data/output/InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_false/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/home2/data/images/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/home2/data/images/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'InceptionV3_decoder_dim_512_fine_tune_encoder_tru_fine_tune_embeddings_false'  # any name you want
+    dataset_basename = 'InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_false'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -40,10 +40,10 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'InceptionV3_decoder_dim_512_fine_tune_encoder_tru_fine_tune_embeddings_false'  # any name you want
+    model_basename = 'InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_false'  # any name you want
 
     # training parameters
-    epochs = 20
+    epochs = 50
     batch_size = 100
     pretrained_encoder = 'InceptionV3'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
     fine_tune_encoder = True  # fine-tune encoder or not
