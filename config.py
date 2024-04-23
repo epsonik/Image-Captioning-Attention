658a8e52727384c461e07239c14059fa497907a6
mateuszb <mateusz.b@aibusters.com>
2024-04-23 23:22:58 +0200

    InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class config:
 
     # training parameters
     epochs = 50
-    batch_size = 400
+    batch_size = 200
     pretrained_encoder = 'InceptionV3'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
     fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
