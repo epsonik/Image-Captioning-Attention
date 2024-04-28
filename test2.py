d31f75543771ef54f91cfe070a0dae163de44d8b
mateuszb <mateusz.b@aibusters.com>
2024-04-28 18:15:21 +0200

    InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false - evaluation

--- test2.py
+++ test2.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
 
             def temp(beam_size, report_name):
                 print("Scores for ", data_name)
-                (bleu1, bleu2, bleu3, bleu4), cider, rouge = evaluate(encoder, decoder, "att2all", beam_size)
+                (bleu1, bleu2, bleu3, bleu4), cider, rouge = evaluate(encoder, decoder, caption_model, beam_size)
 
                 print("\nScores @ beam size of %d are:" % beam_size)
                 print("   BLEU-1: %.4f" % bleu1)
