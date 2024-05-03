9c1134ec9c6a8a5ceba1f6bfa5ce2dcd371b17ea
mateuszb <mateusz.b@aibusters.com>
2024-05-03 13:53:13 +0200

    show_tell_InceptionV3_decoder_dim_256_fine_tune_encoder_true_fine_tune_embeddings_true - evaluation

--- test2.py
+++ test2.py
@@ -166,8 +166,8 @@ if __name__ == '__main__':
 
     configs = dict()
     output_path2 = [
-        "best_checkpoint_adaptive_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true-epoch-36.pth.tar"]
-    output_path = ["adaptive_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true"]
+        "best_checkpoint_show_tell_InceptionV3_decoder_dim_256_fine_tune_encoder_true_fine_tune_embeddings_true-epoch-42.pth.tar"]
+    output_path = ["show_tell_InceptionV3_decoder_dim_256_fine_tune_encoder_true_fine_tune_embeddings_true"]
     cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
     for data_name in output_path:
