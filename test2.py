1aa971a7a529587f70d1523dad823a1c4e540b9a
mateuszb <mateusz.b@aibusters.com>
2024-05-25 22:22:15 +0200

    adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true eval

--- test2.py
+++ test2.py
@@ -207,7 +207,6 @@ if __name__ == '__main__':
 
     configs = dict()
     output_path2 = [
-        "best_checkpoint_adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true-epoch-17.pth.tar",
         "best_checkpoint_adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-6.pth.tar"]
     output_path = ["adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true"]
     cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
@@ -249,8 +248,8 @@ if __name__ == '__main__':
                 generate_report(report_name, model_name, beam_size, bleu1, bleu2, bleu3, bleu4, cider, rouge)
 
 
-            temp(1, "final_results_k1.csv")
-            temp(2, "final_results_k2.csv")
+            # temp(1, "final_results_k1.csv")
+            # temp(2, "final_results_k2.csv")
             temp(3, "final_results_k3.csv")
             temp(5, "final_results_k5.csv")
             temp(8, "final_results_k8.csv")
