1eee24b6ed93d28382c1d6dc537e78aaa9d1afc2
mateuszb <mateusz.b@aibusters.com>
2024-05-28 15:41:47 +0200

    spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false

--- models/decoders/adaptive_att.py
+++ models/decoders/adaptive_att.py
@@ -313,6 +313,9 @@ class Decoder(BasicDecoder):
         # Input is word embedding concatenated with global image feature,
         # so the size of input should be embed_dim * 2 ([ w_t; v^g ] => embed_dim * 2)
         self.decode_step = AdaptiveLSTMCell(embed_dim * 2, decoder_dim)  # LSTM with visual sentinel
+        print("caption_model")
+        print(self.caption_model)
+        print(caption_model)
         self.adaptive_attention = AdaptiveAttention(attention_dim, decoder_dim, caption_model)
 
     def init_hidden_state(self, spatial_feature: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
