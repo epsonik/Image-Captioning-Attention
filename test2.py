f311627d8a0430378904349616caf0ebc2f1742f
mateuszb <mateusz.b@aibusters.com>
2024-05-31 00:30:31 +0200

    best_checkpoint_spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-10.pth.tar

--- test2.py
+++ test2.py
@@ -207,7 +207,6 @@ if __name__ == '__main__':
 
     configs = dict()
     output_path2 = [
-        "best_checkpoint_spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true-epoch-36.pth.tar",
         "best_checkpoint_spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-10.pth.tar"]
     output_path = ["spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true"]
     cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
