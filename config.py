847ecb05b931b6f40ee8ebdafd283aa4f3f189c9
mateuszb <mateusz.b@aibusters.com>
2024-05-30 23:44:10 +0200

    checkpoint_adaptive_Regnet16_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-29.pth.tar

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import os
 
 class config:
     # global parameters
-    cuda_device = "cuda:0"
+    cuda_device = "cuda:1"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
-    caption_model = 'spatial_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
+    caption_model = 'adaptive_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true/"
+    output_path = "data/output/adaptive_Regnet16_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/home2/data/images/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/home2/data/images/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true'  # any name you want
+    dataset_basename = 'adaptive_Regnet16_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -40,18 +40,18 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true'  # any name you want
+    model_basename = 'adaptive_Regnet16_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true'  # any name you want
 
     # training parameters
     epochs = 50
-    batch_size = 50
-    pretrained_encoder = 'InceptionV3'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152 InceptionV3
+    batch_size = 10
+    pretrained_encoder = 'Regnet16'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152 InceptionV3
     fine_tune_encoder = True  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
     checkpoint = os.path.join(base_path, output_path,
-                              'checkpoints/checkpoint_spatial_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-29.pth.tar')  # path to load checkpoint, None if none
+                              'checkpoints/checkpoint_adaptive_Regnet16_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-29.pth.tar')  # path to load checkpoint, None if none
     # checkpoint = None
     workers = 0  # num_workers in dataloader
     tau = 1.  # penalty term τ for doubly stochastic attention in paper: show, attend and tell
