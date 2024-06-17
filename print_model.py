8aac60711f73c90cc8599d9451c60cae7e28be8e
mateuszb <mateusz.b@aibusters.com>
2024-06-17 21:24:24 +0200

    Resnet152

--- print_model.py
+++ print_model.py
@@ -3,8 +3,7 @@ import torchinfo
 print("inception")
 inception = torchvision.models.inception_v3(pretrained=True)
 modules = list(inception.children())[:-3]
-print(inception)
-print(torchinfo.summary(inception, (3, 224, 224), batch_dim=0,
+print(torchinfo.summary(modules, (3, 224, 224), batch_dim=0,
                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 # modules = nn.Sequential(*modules)
 # print(torchinfo.summary(modules, (3, 224, 224), batch_dim=0,
