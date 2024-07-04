360be719e0777587cbb86963667849f21bfc86d4
mateuszb <mateusz.b@aibusters.com>
2024-07-04 22:47:39 +0200

    att2all_Resnet152_decoder_dim_512_attention_dim_Resnet152_fine_tune_encoder_false_fine_tune_embeddings_false

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ class config:
     # global parameters
     cuda_device = "cuda:1"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
-    caption_model = 'spatial_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
+    caption_model = 'att2all'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/spatial_att_DenseNet201_decoder_dim_512_attention_dim_512_fine_tune_encoder_true_fine_false_embeddings_true/"
+    output_path = "data/output/att2all_Resnet152_decoder_dim_512_attention_dim_Resnet152_fine_tune_encoder_false_fine_tune_embeddings_false/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/home2/data/images/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/home2/data/images/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'spatial_att_DenseNet201_decoder_dim_512_attention_dim_512_fine_tune_encoder_true_fine_false_embeddings_true'  # any name you want
+    dataset_basename = 'att2all_Resnet152_decoder_dim_512_attention_dim_Resnet152_fine_tune_encoder_false_fine_tune_embeddings_false'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -32,7 +32,7 @@ class config:
                               '/home2/data/images/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
     embed_dim = 512  # dimension of word embeddings
     # only makes sense when `embed_pretrain = False`
-    fine_tune_embeddings = True  # fine-tune word embeddings?
+    fine_tune_embeddings = False  # fine-tune word embeddings?
 
     # model parameters
     attention_dim = 512  # dimension of attention network
@@ -40,19 +40,19 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'spatial_att_DenseNet201_decoder_dim_512_attention_dim_512_fine_tune_encoder_true_fine_false_embeddings_true'  # any name you want
+    model_basename = 'att2all_Resnet152_decoder_dim_512_attention_dim_Resnet152_fine_tune_encoder_false_fine_tune_embeddings_false'  # any name you want
 
     # training parameters
-    epochs = 50
-    batch_size = 20
-    pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
-    fine_tune_encoder = True  # fine-tune encoder or not
+    epochs = 30
+    batch_size = 400
+    pretrained_encoder = 'Resnet152'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
+    fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
-    checkpoint = os.path.join(base_path, output_path,
-                              'checkpoints/checkpoint_spatial_att_DenseNet201_decoder_dim_512_attention_dim_512_fine_tune_encoder_false_fine_false_embeddings_false-epoch-29.pth.tar')  # path to load checkpoint, None if none
-    # checkpoint = None
+    # checkpoint = os.path.join(base_path, output_path,
+    #                           'checkpoints/checkpoint_spatial_att_DenseNet201_decoder_dim_512_attention_dim_512_fine_tune_encoder_false_fine_false_embeddings_false-epoch-29.pth.tar')  # path to load checkpoint, None if none
+    checkpoint = None
     workers = 0  # num_workers in dataloader
     tau = 1.  # penalty term τ for doubly stochastic attention in paper: show, attend and tell
     # you only need to set this when 'caption_model' is set to 'att2all'
