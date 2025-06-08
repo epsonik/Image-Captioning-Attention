6b844e752c0387f514c4fc8bce3eb3839f30aab0
mateuszb <mateusz.b@aibusters.com>
2025-06-08 14:57:41 +0200

    IMG_4276.JPG

--- utils/visual.py
+++ utils/visual.py
@@ -84,7 +84,7 @@ def visualize_att_beta(
         # images
         plt.imshow(image)
         # words of sentence
-        plt.text(0, 100, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=10,
+        plt.title('%s' % (words[t]), color='black', backgroundcolor='white', fontsize=10,
                  horizontalalignment='center', verticalalignment='center')
 
         # alphas
