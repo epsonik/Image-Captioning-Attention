3a6e0f0f052001431410bf56d9881757b2c531ca
mateuszb <mateusz.b@aibusters.com>
2025-06-16 09:11:26 +0200

    adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false_fastText

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import os
 
 class config:
     # global parameters
-    cuda_device = "cuda:1"
+    cuda_device = "cuda:0"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
     caption_model = 'adaptive_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/Resnet101_p/"
+    output_path = "data/output/adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false_fastText/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'Resnet101_p'  # any name you want
+    dataset_basename = 'adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false_fastText'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -26,10 +26,12 @@ class config:
     # with length lower than this value will be padded from right side to fit this length
 
     # word embeddings parameters
-    embed_pretrain = False  # false: initialize embedding weights randomly
+    embed_pretrain = True  # false: initialize embedding weights randomly
     # true: load pre-trained word embeddings
+    # embed_path = os.path.join(base_path,
+    #                           '/mnt/dysk2/dane/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
     embed_path = os.path.join(base_path,
-                              '/mnt/dysk2/dane/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
+                              '/mnt/dysk2/dane/fastText/wiki-news-300d-1M-subword.vec')  # only makes sense when `embed_pretrain = True`
     embed_dim = 300  # dimension of word embeddings
     # only makes sense when `embed_pretrain = False`
     fine_tune_embeddings = False  # fine-tune word embeddings?
@@ -40,13 +42,13 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'Resnet101_p'  # any name you want
+    model_basename = 'adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false_fastText'  # any name you want
 
     # training parameters
-    epochs = 50
-    batch_size = 10
-    pretrained_encoder = 'Resnet101'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
-    fine_tune_encoder = True  # fine-tune encoder or not
+    epochs = 30
+    batch_size = 500
+    pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
+    fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
