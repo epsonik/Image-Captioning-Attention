dc50d4199fd122928fe23e41fa73db929b381580
mateuszb <mateusz.b@aibusters.com>
2025-06-16 08:40:22 +0200

    adaptive_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false

--- test2.py
+++ test2.py
@@ -55,7 +55,7 @@ def evaluate(encoder, decoder, caption_model, beam_size: int) -> float:
     loader = DataLoader(
         CaptionDataset(
             os.path.join(data_f,
-                         'output/adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true'),
+                         'output/adaptive_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false'),
             data_name, 'test',
             transform=transforms.Compose([normalize])
         ),
@@ -206,7 +206,7 @@ def generate_report_for_all_models(results_path):
 if __name__ == '__main__':
 
     configs = dict()
-    output_path = ["adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true"]
+    output_path = ["adaptive_InceptionV3_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false"]
     cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
     for data_name in output_path:
