321784f4f543135be658819f88295705d464e4d3
mateuszb <mateusz.b@aibusters.com>
2025-06-17 16:43:32 +0200

    DenseNet201_glove300_decoder_dim_128_attention_dim_128_ft_embeddings_false_fine_tune_encoder_true

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import os
 
 class config:
     # global parameters
-    cuda_device = "cuda:1"
+    cuda_device = "cuda:0"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
     caption_model = 'att2all'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/DenseNet201_glove300_decoder_dim_512_attention_dim_128_ft_embeddings_true_fine_tune_encoder_false/"
+    output_path = "data/output/DenseNet201_glove300_decoder_dim_128_attention_dim_128_ft_embeddings_false_fine_tune_encoder_true/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'DenseNet201_glove300_decoder_dim_512_attention_dim_128_ft_embeddings_true_fine_tune_encoder_false'  # any name you want
+    dataset_basename = 'DenseNet201_glove300_decoder_dim_128_attention_dim_128_ft_embeddings_false_fine_tune_encoder_true'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -32,7 +32,7 @@ class config:
                               '/mnt/dysk2/dane/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
     embed_dim = 300  # dimension of word embeddings
     # only makes sense when `embed_pretrain = False`
-    fine_tune_embeddings = True  # fine-tune word embeddings?
+    fine_tune_embeddings = False  # fine-tune word embeddings?
 
     # model parameters
     attention_dim = 128  # dimension of attention network
@@ -40,13 +40,13 @@ class config:
     decoder_dim = 128  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'DenseNet201_glove300_decoder_dim_512_attention_dim_128_ft_embeddings_true_fine_tune_encoder_false'  # any name you want
+    model_basename = 'DenseNet201_glove300_decoder_dim_128_attention_dim_128_ft_embeddings_false_fine_tune_encoder_true'  # any name you want
 
     # training parameters
     epochs = 50
     batch_size = 100
     pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
-    fine_tune_encoder = False  # fine-tune encoder or not
+    fine_tune_encoder = True  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
