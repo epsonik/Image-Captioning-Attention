8d437a09e58017811dd48f05c23ccb5b17b44302
mateuszb <mateusz.b@aibusters.com>
2025-06-20 11:08:17 +0200

    spatial_DenseNet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stagest

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import os
 
 class config:
     # global parameters
-    cuda_device = "cuda:1"
+    cuda_device = "cuda:0"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
-    caption_model = 'adaptive_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
+    caption_model = 'spatial_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true_fastText/"
+    output_path = "data/output/spatial_DenseNet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true_fastText'  # any name you want
+    dataset_basename = 'spatial_DenseNet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -26,12 +26,12 @@ class config:
     # with length lower than this value will be padded from right side to fit this length
 
     # word embeddings parameters
-    embed_pretrain = True  # false: initialize embedding weights randomly
+    embed_pretrain = False  # false: initialize embedding weights randomly
     # true: load pre-trained word embeddings
-    # embed_path = os.path.join(base_path,
-    #                           '/mnt/dysk2/dane/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
     embed_path = os.path.join(base_path,
-                              '/mnt/dysk2/dane/fastText/wiki-news-300d-1M-subword.vec')  # only makes sense when `embed_pretrain = True`
+                              '/mnt/dysk2/dane/glove/glove.6B.300d.txt')  # only makes sense when `embed_pretrain = True`
+    # embed_path = os.path.join(base_path,
+    #                           '/mnt/dysk2/dane/fastText/wiki-news-300d-1M-subword.vec')  # only makes sense when `embed_pretrain = True`
     embed_dim = 300  # dimension of word embeddings
     # only makes sense when `embed_pretrain = False`
     fine_tune_embeddings = True  # fine-tune word embeddings?
@@ -42,19 +42,19 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true_fastText'  # any name you want
+    model_basename = 'spatial_DenseNet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
 
     # training parameters
-    epochs = 50
-    batch_size = 40
+    epochs = 30
+    batch_size = 450
     pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
-    fine_tune_encoder = True  # fine-tune encoder or not
+    fine_tune_encoder = False  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
     grad_clip = 5.  # gradient threshold in clip gradients
-    checkpoint = os.path.join(base_path, output_path,
-                              'checkpoints/checkpoint_adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true_fastText-epoch-22.pth.tar')  # path to load checkpoint, None if none
-    # checkpoint = None
+    # checkpoint = os.path.join(base_path, output_path,
+    #                           'checkpoints/checkpoint_adaptive_DenseNet201_decoder_dim_512_fine_tune_encoder_true_fine_tune_embeddings_true_fastText-epoch-22.pth.tar')  # path to load checkpoint, None if none
+    checkpoint = None
     workers = 0  # num_workers in dataloader
     tau = 1.  # penalty term τ for doubly stochastic attention in paper: show, attend and tell
     # you only need to set this when 'caption_model' is set to 'att2all'
