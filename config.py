339e15f5dbe649f619f7bd344688dc9fad1d4bed
mateuszb <mateusz.b@aibusters.com>
2025-06-26 20:01:25 +0200

    adaptive_att_50_Densenet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ class config:
     # global parameters
     cuda_device = "cuda:0"
     base_path = os.path.abspath(os.path.dirname(__file__))  # path to this project
-    caption_model = 'att2all'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
+    caption_model = 'adaptive_att'  # 'show_tell', 'att2all', 'adaptive_att', 'spatial_att'
     # refer to README.md for more info about each model
-    output_path = "data/output/att2all_50_Resnet101_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages/"
+    output_path = "data/output/adaptive_att_50_Densenet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages/"
     dataset_type = 'coco'
     # dataset parameters
     dataset_image_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/')
     dataset_caption_path = os.path.join(base_path, '/mnt/dysk2/dane/coco2014/karpathy/dataset_coco.json')
     dataset_output_path = os.path.join(base_path, output_path)  # folder with data files saved by preprocess.py
-    dataset_basename = 'att2all_50_Resnet101_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
+    dataset_basename = 'adaptive_att_50_Densenet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
 
     # preprocess parameters
     captions_per_image = 5
@@ -42,12 +42,12 @@ class config:
     decoder_dim = 512  # dimension of decoder's hidden layer
     dropout = 0.5
     model_path = os.path.join(base_path, output_path, 'checkpoints/')  # path to save checkpoints
-    model_basename = 'att2all_50_Resnet101_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
+    model_basename = 'adaptive_att_50_Densenet201_decoder_dim_512_attention_dim_128_fine_tune_encoder_true_no_emb_all_stages'  # any name you want
 
     # training parameters
     epochs = 50
-    batch_size = 10
-    pretrained_encoder = 'Resnet101'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
+    batch_size = 15
+    pretrained_encoder = 'DenseNet201'  # DenseNet201 Resnet101 Regnet32 DenseNet121 DenseNet161 Resnet152
     fine_tune_encoder = True  # fine-tune encoder or not
     encoder_lr = 1e-4  # learning rate of encoder (if fine-tune)
     decoder_lr = 4e-4  # learning rate of decoder
