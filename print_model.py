52ede23ece5d3e4de5860694b790decbde545e0c
mateuszb <mateusz.b@aibusters.com>
2025-06-29 17:27:58 +0200

    print model

--- print_model.py
+++ print_model.py
@@ -28,6 +28,6 @@ densenet201 = torchvision.models.densenet201(pretrained=True)
 print(torchinfo.summary(densenet201, (3, 224, 224), batch_dim=0,
                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 print("regnet")
-densenet201 = torchvision.models.regnet_y_16gf(weights="IMAGENET1K_SWAG_E2E_V1")
-print(torchinfo.summary(densenet201, (3, 224, 224), batch_dim=0,
+regnet = torchvision.models.regnet_y_16gf(weights="IMAGENET1K_SWAG_E2E_V1")
+print(torchinfo.summary(regnet, (3, 224, 224), batch_dim=0,
                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
