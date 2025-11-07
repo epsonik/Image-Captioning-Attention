96c3ec2c0b063dc74db86b45bceaf5e6225d290e
mateuszb <mateusz.b@aibusters.com>
2025-11-07 23:19:13 +0100

    print model

--- utils/visual.py
+++ utils/visual.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, Union
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import skimage.transform
@@ -7,7 +7,8 @@ import numpy as np
 import os
 
 def visualize_att_beta(
-    image_path: str,
+    image_data: Union[str, np.ndarray],
+    original_image_path: str,
     seq: list,
     alphas: list,
     rev_word_map: Dict[int, str],
@@ -20,8 +21,11 @@ def visualize_att_beta(
 
     Parameters
     ----------
-    image_path : str
-        Path to image that has been captioned
+    image_data : Union[str, np.ndarray]
+        Path to image or image as a numpy array.
+
+    original_image_path : str
+        Path to the original image, used for saving the visualization.
 
     seq : list
         Generated caption on the above mentioned image using beam search
@@ -38,7 +42,11 @@ def visualize_att_beta(
     smooth : bool, optional, default=True
         Smooth weights or not?
     """
-    image = Image.open(image_path)
+    if isinstance(image_data, str):
+        image = Image.open(image_data)
+    else:
+        image = Image.fromarray(image_data)
+
     image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
 
     words = [rev_word_map[ind] for ind in seq]
@@ -63,19 +71,6 @@ def visualize_att_beta(
     plt.imshow(image)
     plt.axis('off')
 
-    # betas' curve
-    if betas is not None:
-        plt.subplot(grid[0 : fig_height - 1, img_size : fig_width])
-
-        x = range(1, len(words), 1)
-        y = [ (1 - betas[t].item()) for t in range(1, len(words)) ]
-
-        for a, b in zip(x, y):
-            plt.text(a + 0.05, b + 0.05, '%.2f' % b, ha='center', va='bottom', fontsize=12)
-
-        plt.axis('off')
-        plt.plot(x, y)
-
     for t in range(1, len(words)):
         if t > 50:
             break
@@ -99,15 +94,16 @@ def visualize_att_beta(
 
         plt.axis('off')
 
-    output_dir = os.path.dirname(image_path)
+    output_dir = os.path.dirname(original_image_path)
     cleaned_model_name = model_name.replace('best_checkpoint_', '').replace('.pth.tar', '').replace('checkpoint_', '')
-    image_basename = os.path.splitext(os.path.basename(image_path))[0]
+    image_basename = os.path.splitext(os.path.basename(original_image_path))[0]
     save_path = os.path.join(output_dir, f"{cleaned_model_name}_{image_basename}.png")
     plt.savefig(save_path)
     plt.close(fig)
 
 def visualize_att(
-    image_path: str,
+    image_data: Union[str, np.ndarray],
+    original_image_path: str,
     seq: list,
     alphas: list,
     rev_word_map: Dict[int, str],
@@ -121,8 +117,11 @@ def visualize_att(
 
     Parameters
     ----------
-    image_path : str
-        Path to image that has been captioned
+    image_data : Union[str, np.ndarray]
+        Path to image or image as a numpy array.
+
+    original_image_path : str
+        Path to the original image, used for saving the visualization.
 
     seq : list
         Generated caption on the above mentioned image using beam search
@@ -136,7 +135,11 @@ def visualize_att(
     smooth : bool, optional, default=True
         Smooth weights or not?
     """
-    image = Image.open(image_path)
+    if isinstance(image_data, str):
+        image = Image.open(image_data)
+    else:
+        image = Image.fromarray(image_data)
+
     image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
 
     words = [rev_word_map[ind] for ind in seq]
@@ -175,9 +178,9 @@ def visualize_att(
         plt.set_cmap(cm.Greys_r)
         plt.axis('off')
 
-    output_dir = os.path.dirname(image_path)
+    output_dir = os.path.dirname(original_image_path)
     cleaned_model_name = model_name.replace('best_checkpoint_', '').replace('.pth.tar', '').replace('checkpoint_', '')
-    image_basename = os.path.splitext(os.path.basename(image_path))[0]
+    image_basename = os.path.splitext(os.path.basename(original_image_path))[0]
     save_path = os.path.join(output_dir, f"{cleaned_model_name}_{image_basename}.png")
     plt.savefig(save_path)
     plt.close(fig)
