7659bd686b4a698caa26f0c4eadac543cc9bee24
mateuszb <mateusz.b@aibusters.com>
2025-11-07 21:51:09 +0100

    print model

--- utils/visual.py
+++ utils/visual.py
@@ -6,7 +6,6 @@ import skimage.transform
 from PIL import Image
 import numpy as np
 
-
 def visualize_att_beta(
     image_path: str,
     seq: list,
@@ -17,38 +16,20 @@ def visualize_att_beta(
 ) -> None:
     """
     Visualize caption with weights and betas at every word.
-
-    Parameters
-    ----------
-    image_path : str
-        Path to image that has been captioned
-
-    seq : list
-        Generated caption on the above mentioned image using beam search
-
-    alphas : list
-        Attention weights at each time step
-
-    betas : list
-        Sentinel gate at each time step (only in 'adaptive_att' mode)
-
-    rev_word_map : Dict[int, str]
-        Reverse word mapping, i.e. ix2word
-
-    smooth : bool, optional, default=True
-        Smooth weights or not?
+    Fixed: ensure full image is shown in each subplot by using a numpy array,
+    explicit extent and aspect='auto' for both image and alpha overlays.
     """
     image = Image.open(image_path)
     image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
+    image_np = np.array(image)
+    h, w = image_np.shape[:2]
 
     words = [rev_word_map[ind] for ind in seq]
 
-    # subplot settings
     num_col = len(words) - 1
     num_row = 1
     subplot_size = 4
 
-    # graph settings
     fig = plt.figure(dpi=100)
     fig.set_size_inches(subplot_size * num_col, subplot_size * num_row)
 
@@ -59,45 +40,41 @@ def visualize_att_beta(
     grid = plt.GridSpec(fig_height, fig_width)
 
     # big image
-    plt.subplot(grid[0: img_size, 0: img_size])
-    plt.imshow(image)
-    plt.axis('off')
+    ax = plt.subplot(grid[0: img_size, 0: img_size])
+    ax.imshow(image_np, aspect='auto', extent=(0, w, h, 0))
+    ax.axis('off')
 
-    # betas' curve
-    # if betas is not None:
-    #     plt.subplot(grid[0: fig_height - 1, img_size: fig_width])
-    #
-    #     x = range(1, len(words), 1)
-    #     y = [(1 - betas[t].item()) for t in range(1, len(words))]
-    #
-    #     for a, b in zip(x, y):
-    #         plt.text(a + 0.05, b + 0.05, '%.2f' % b, ha='center', va='bottom', fontsize=12)
-    #
-    #     plt.axis('off')
-    #     plt.plot(x, y)
+    if betas is not None:
+        axb = plt.subplot(grid[0: fig_height - 1, img_size: fig_width])
+        x = range(1, len(words), 1)
+        y = [(1 - betas[t].item()) for t in range(1, len(words))]
+
+        for a, b in zip(x, y):
+            axb.text(a + 0.05, b + 0.05, '%.2f' % b, ha='center', va='bottom', fontsize=12)
+
+        axb.axis('off')
+        axb.plot(x, y)
 
     for t in range(1, len(words)):
         if t > 50:
             break
 
-        plt.subplot(grid[fig_height - 1, img_size + t - 1])
-        # images
-        plt.imshow(image)
-        # words of sentence
-        plt.title('%s' % (words[t]), color='black', fontsize=10,
-                 horizontalalignment='center', verticalalignment='center')
+        ax = plt.subplot(grid[fig_height - 1, img_size + t - 1])
+        ax.imshow(image_np, aspect='auto', extent=(0, w, h, 0))
+        ax.set_title('%s' % (words[t]), color='black', fontsize=10,
+                     horizontalalignment='center', verticalalignment='center')
 
-        # alphas
         current_alpha = alphas[t, :]
         if smooth:
             alpha = skimage.transform.pyramid_expand(current_alpha.numpy(), upscale=24, sigma=8)
         else:
             alpha = skimage.transform.resize(current_alpha.numpy(), [14 * 24, 14 * 24])
-        plt.imshow(alpha, alpha=0.6)
-        plt.set_cmap('jet')
 
-        plt.axis('off')
+        # overlay with same extent so it covers whole image
+        ax.imshow(alpha, alpha=0.6, cmap='jet', extent=(0, w, h, 0), interpolation='bilinear')
+        ax.axis('off')
 
+    plt.tight_layout()
     head, tail = os.path.split(image_path)
     plt.savefig(os.path.join(head, "att_" + tail), bbox_inches='tight')
 
@@ -111,37 +88,20 @@ def visualize_att(
 ) -> None:
     """
     Visualize caption with weights at every word.
-
-    Adapted from: https://github.com/kelvinxu/arctic-captions/blob/master/alpha_visualization.ipynb
-
-    Parameters
-    ----------
-    image_path : str
-        Path to image that has been captioned
-
-    seq : list
-        Generated caption on the above mentioned image using beam search
-
-    alphas : list
-        Attention weights at each time step
-
-    rev_word_map : Dict[int, str]
-        Reverse word mapping, i.e. ix2word
-
-    smooth : bool, optional, default=True
-        Smooth weights or not?
+    Fixed: ensure full image is shown in each subplot by using a numpy array,
+    explicit extent and aspect='auto' for both image and alpha overlays.
     """
     image = Image.open(image_path)
     image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
+    image_np = np.array(image)
+    h, w = image_np.shape[:2]
 
     words = [rev_word_map[ind] for ind in seq]
 
-    # subplot settings
     num_col = 5
     num_row = int(np.ceil(len(words) / float(num_col)))
     subplot_size = 4
 
-    # graph settings
     fig = plt.figure(dpi=100)
     fig.set_size_inches(subplot_size * num_col, subplot_size * num_row)
 
@@ -149,11 +109,9 @@ def visualize_att(
         if t > 50:
             break
 
-        plt.subplot(num_row, num_col, t + 1)
-
-        plt.text(0, 1, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=12)
-
-        plt.imshow(image)
+        ax = plt.subplot(num_row, num_col, t + 1)
+        ax.text(0, 1, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=12)
+        ax.imshow(image_np, aspect='auto', extent=(0, w, h, 0))
 
         current_alpha = alphas[t, :]
 
@@ -163,11 +121,12 @@ def visualize_att(
             alpha = skimage.transform.resize(current_alpha.numpy(), [14 * 24, 14 * 24])
 
         if t == 0:
-            plt.imshow(alpha, alpha=0)
+            ax.imshow(alpha, alpha=0, cmap=cm.Greys_r, extent=(0, w, h, 0), interpolation='bilinear')
         else:
-            plt.imshow(alpha, alpha=0.8)
+            ax.imshow(alpha, alpha=0.8, cmap=cm.Greys_r, extent=(0, w, h, 0), interpolation='bilinear')
 
-        plt.set_cmap(cm.Greys_r)
-        plt.axis('off')
+        ax.axis('off')
+
+    plt.tight_layout()
     head, tail = os.path.split(image_path)
     plt.savefig(os.path.join(head, "att_" + tail), bbox_inches='tight')
