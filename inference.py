4e2a4298007068b8e03c7222266ecd041a1ef710
mateuszb <mateusz.b@aibusters.com>
2025-11-07 16:29:23 +0100

    print model

--- inference.py
+++ inference.py
@@ -96,62 +96,72 @@ def generate_caption(
 
 
 if __name__ == '__main__':
-    output_path = "att2all/att2all_DenseNet161_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false"
-    img = "./assets/IMG_4276.JPG"
-    model_names = [
-        "best_checkpoint_att2all_DenseNet161_decoder_dim_512_fine_tune_encoder_false_fine_tune_embeddings_false-epoch-7.pth.tar"
-    ]
+    assets_dir = "./assets"
+    # set folder containing checkpoints
+    model_dir = "/home/bartosiewicz/mateusz/Image-Captioning-Attention/data/output/t"
 
-    model_path = os.path.join(data_f, "output", output_path, "checkpoints")
+    # gather all files ending with '.pth.tar' (files only), sorted
+    model_names = sorted([
+        f for f in os.listdir(model_dir)
+        if os.path.isfile(os.path.join(model_dir, f)) and f.endswith('.pth.tar')
+    ])
+
+    # use that folder as the model_path (replaces previous model_path assignment)
+    model_path = model_dir
+    model_path = os.path.join(data_f, "output")
+    beam_size = 3
+    ifsmooth = True
+
+    # gather image files from `./assets`
+    exts = {'.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff'}
+    images = sorted([f for f in os.listdir(assets_dir) if os.path.splitext(f)[1].lower() in exts])
 
     for model_name in model_names:
-        checkpoint_path = os.path.join(model_path, model_name)  # model checkpoint
-        beam_size = 3
-        ifsmooth = True
-
-        # load model
+        checkpoint_path = os.path.join(model_path,model_name)
+        # load model once per checkpoint
         checkpoint = torch.load(checkpoint_path, map_location=str(device))
 
-        decoder = checkpoint['decoder']
-        decoder = decoder.to(device)
+        decoder = checkpoint['decoder'].to(device)
         decoder.eval()
 
-        encoder = checkpoint['encoder']
-        encoder = encoder.to(device)
+        encoder = checkpoint['encoder'].to(device)
         encoder.eval()
 
         caption_model = checkpoint['caption_model']
 
-        # encoder-decoder with beam search
-        if caption_model == 'show_tell':
-            seq = generate_caption(encoder, decoder, img, word_map, caption_model, beam_size)
-            caption = [rev_word_map[ind] for ind in seq if
-                       ind not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]
-            print('Caption: ', ' '.join(caption))
+        for img_fname in images:
+            img_path = os.path.join(assets_dir, img_fname)
 
-        elif caption_model == 'att2all' or caption_model == 'spatial_att':
-            seq, alphas = generate_caption(encoder, decoder, img, word_map, caption_model, beam_size)
-            prediction = [w for w in seq if w not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]
-            alphas = torch.FloatTensor(alphas)
-            # visualize caption and attention of best sequence
-            visualize_att(
-                image_path=img,
-                seq=seq,
-                rev_word_map=rev_word_map,
-                alphas=alphas,
-                model_name=model_name,
-                smooth=ifsmooth
-            )
+            if caption_model == 'show_tell':
+                seq = generate_caption(encoder, decoder, img_path, word_map, caption_model, beam_size)
+                caption = [rev_word_map[ind] for ind in seq if
+                           ind not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]
+                print('Image:', img_fname, 'Model:', model_name, 'Caption:', ' '.join(caption))
 
-        elif caption_model == 'adaptive_att':
-            seq, alphas, betas = generate_caption(encoder, decoder, img, word_map, caption_model, beam_size)
-            alphas = torch.FloatTensor(alphas)
-            visualize_att_beta(
-                image_path=img,
-                seq=seq,
-                rev_word_map=rev_word_map,
-                alphas=alphas,
-                betas=betas,
-                model_name=model_name,
-                smooth=ifsmooth
-            )
+            elif caption_model in ('att2all', 'spatial_att'):
+                seq, alphas = generate_caption(encoder, decoder, img_path, word_map, caption_model, beam_size)
+                prediction = [w for w in seq if w not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]
+                alphas = torch.FloatTensor(alphas)
+                visualize_att(
+                    image_path=img_path,
+                    seq=seq,
+                    rev_word_map=rev_word_map,
+                    alphas=alphas,
+                    model_name=model_name,
+                    smooth=ifsmooth
+                )
+                print('Saved attention visualization for:', img_fname, 'Model:', model_name)
+
+            elif caption_model == 'adaptive_att':
+                seq, alphas, betas = generate_caption(encoder, decoder, img_path, word_map, caption_model, beam_size)
+                alphas = torch.FloatTensor(alphas)
+                visualize_att_beta(
+                    image_path=img_path,
+                    seq=seq,
+                    rev_word_map=rev_word_map,
+                    alphas=alphas,
+                    betas=betas,
+                    model_name=model_name,
+                    smooth=ifsmooth
+                )
+                print('Saved adaptive attention visualization for:', img_fname, 'Model:', model_name)
